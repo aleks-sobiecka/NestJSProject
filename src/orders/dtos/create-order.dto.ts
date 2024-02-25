b50147443f137b64662fbc7fb66728b4e90ecf4e
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2024-02-25 23:21:00 +0100

    Added endpoints for orders

---
+++ src/orders/dtos/create-order.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+import { Product } from 'src/db';
+
+export class CreateOrderDTO {
+  @IsNotEmpty()
+  @IsString()
+  productId: Product['id'];
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(5, 20)
+  client: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(10, 20)
+  address: string;
+}
